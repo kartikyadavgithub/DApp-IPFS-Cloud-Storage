d051451c35738fe91f79c096bff21c676e6f01b6
Kartik Yadav <kartikyadav@MacBook-Air.local>
2022-07-25 04:45:20 +0530

    first commit

--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -1,37 +1,69 @@
 pragma solidity ^0.5.0;
 
 contract DStorage {
-  // Name
-  // Number of files
-  // Mapping fileId=>Struct 
 
-  // Struct
-
-
-  // Event
+  string public name = 'DStorage';
+  uint public fileCount = 0;                    // Number of files
+  mapping (uint => File) public files;          // Mapping fileId=>Struct
+  struct File{                                  // Struct
+    uint fileId;
+    string fileHash;
+    uint fileSize;
+    string fileType;
+    string fileName;
+    string fileDescription;
+    uint uploadTime;
+    address payable uploader;
+  }
+ 
+    // Event
+   event FileUploaded(                       
+    uint fileId,
+    string fileHash,
+    uint fileSize,
+    string fileType,
+    string fileName, 
+    string fileDescription,
+    uint uploadTime,
+    address payable uploader
+  );
+  
 
   constructor() public {
   }
 
-  // Upload File function
+  function uploadFile(string memory _fileHash,       // Upload File function
+                      uint _fileSize,
+                      string memory _fileType, 
+                      string memory _fileName, 
+                      string memory _fileDescription) public {
 
-    // Make sure the file hash exists
+    require(bytes(_fileHash).length > 0);            // Make sure the file hash exists
+    require(bytes(_fileType).length > 0);            // Make sure file type exists
+    require(bytes(_fileDescription).length > 0);     // Make sure file description exists
+    require(bytes(_fileName).length > 0);            // Make sure file fileName exists
+    require(msg.sender!=address(0));                 // Make sure uploader address exists
+    require(_fileSize>0);                            // Make sure file size is more than 0
 
-    // Make sure file type exists
+    fileCount  = fileCount +1;                       // Increment file id
 
-    // Make sure file description exists
-
-    // Make sure file fileName exists
-
-    // Make sure uploader address exists
-
-    // Make sure file size is more than 0
-
-
-    // Increment file id
-
-    // Add File to the contract
+    files[fileCount] = File(fileCount,
+                            _fileHash,               // Add File to the contract
+                            _fileSize, 
+                            _fileType, 
+                            _fileName,
+                            _fileDescription, 
+                              now , msg.sender);
 
     // Trigger an event
+    emit FileUploaded(fileCount, 
+                      _fileHash, 
+                      _fileSize, 
+                      _fileType, 
+                      _fileName, 
+                      _fileDescription, 
+                        now, msg.sender);
+
+  }
 
 }
